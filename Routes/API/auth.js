0fbb01198c7594f707ff4a01e41337f093da952c
Nabeelhusein13 <nabeelhusein13>
2023-09-21 12:43:19 -0400

    Auth,Login/out Js code added

--- Routes/API/auth.js
+++ Routes/API/auth.js
@@ -1,1 +1,11 @@
 //* Handles signup, login, logout, and related authentication routes
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
