ea7366c9f337cce51f38bf37ebbb8bc43325c753
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-21 10:25:24 -0400

    added exphdbs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
+
 const app = express();
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
