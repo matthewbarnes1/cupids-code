927cd4561c931b2caf35da3bb13e6c813cc946bb
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-02 15:16:45 -0400

    file name

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
+const sequelize = require('../../config/mattcon');
 const { Profile } = require('../../Models');
 const withAuth = require('../../utils/auth');
 
