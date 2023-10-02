99de53007573d2a340be4b7e438866c6bffc8356
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-02 13:44:11 -0400

    heroku debugging for connection

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const sequelize = require('../../config/connection');
+const sequelize = require('../../config/connection');
 const { Profile } = require('../../Models');
 const withAuth = require('../../utils/auth');
 
