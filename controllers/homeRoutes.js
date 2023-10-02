6d30e20de155aeba83e421cab0dc774efbaaef3a
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-02 10:07:54 -0400

    editing homepage + hyperlinks for login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,51 +4,33 @@ const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+  res.render('homepage', { 
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+  });
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+// router.get('/project/:id', async (req, res) => {
+//   try {
+//     const projectData = await Project.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
 
-    const project = projectData.get({ plain: true });
+//     const project = projectData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('project', {
+//       ...project,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // Use withAuth middleware to prevent access to route
 // router.get('/profile', withAuth, async (req, res) => {
@@ -68,7 +50,20 @@ router.get('/project/:id', async (req, res) => {
 //   } catch (err) {
 //     res.status(500).json(err);
 //   }
-// });
+
+router.get('/', async (req, res) =>{
+  try{
+    res.render('homepage', {
+      profiles: results,
+      logged_in: req.session.logged_in
+    });
+
+    // res.json(results)
+
+  }catch (err){
+
+  }
+})
 
 router.get('/matches', withAuth, async (req, res) =>{
     try{
